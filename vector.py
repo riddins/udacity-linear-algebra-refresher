57a11d15da558f543006aa4969a034ba685f211c
Wayne Iddins <wayne@wayneiddins.net>
2018-09-03 15:09:20 -0400

    merge vector.py with course starter code at: https://storage.googleapis.com/supplemental_media/udacityu/4467878620/vector.py

--- vector.py
+++ vector.py
@@ -12,17 +12,19 @@ class Vector():
         try:
             if not coordinates:
                 raise ValueError
-            self.coordinates = [Decimal(x) for x in coordinates]
+            self.coordinates = tuple(coordinates)
             self.dimension = len(coordinates)
 
         except ValueError:
             raise ValueError('The coordinates must be nonempty')
 
         except TypeError:
-            raise TypeError('The coordinates must be iterable')
+            raise TypeError('The coordinates must be an iterable')
+
 
     def __str__(self):
         return 'Vector: {}'.format(self.coordinates)
+
 
     def __eq__(self, v):
         return self.coordinates == v.coordinates
