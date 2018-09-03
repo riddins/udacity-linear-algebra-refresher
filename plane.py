4b02ea87fc85d80bd461e3bc053547fa305df6a8
Wayne Iddins <wayne@wayneiddins.net>
2018-09-03 14:13:44 -0400

    merged plane.py with course starter code at: https://storage.googleapis.com/supplemental_media/udacityu/4916388673/plane.py

--- plane.py
+++ plane.py
@@ -24,19 +24,78 @@ class Plane:
 
         self.set_basepoint()
 
-    def set_basepoint(self):
-        a = self.normal_vector.coordinates[0]
-        b = self.normal_vector.coordinates[1]
-        c = self.normal_vector.coordinates[2]
 
-        if a != 0:
-            self.basepoint = [self.constant_term / a, 0, 0]
-        elif b != 0:
-            self.basepoint = [0, self.constant_term / b, 0]
-        elif c != 0:
-            self.basepoint = [0, 0, self.constant_term / c]
-        else:
-            raise Exception(self.NO_NONZERO_ELTS_FOUND_MSG)
+    def set_basepoint(self):
+        try:
+            n = self.normal_vector
+            c = self.constant_term
+            basepoint_coords = ['0']*self.dimension
+
+            initial_index = Plane.first_nonzero_index(n)
+            initial_coefficient = n[initial_index]
+
+            basepoint_coords[initial_index] = c/initial_coefficient
+            self.basepoint = Vector(basepoint_coords)
+
+        except Exception as e:
+            if str(e) == Plane.NO_NONZERO_ELTS_FOUND_MSG:
+                self.basepoint = None
+            else:
+                raise e
+
+
+    def __str__(self):
+
+        num_decimal_places = 3
+
+        def write_coefficient(coefficient, is_initial_term=False):
+            coefficient = round(coefficient, num_decimal_places)
+            if coefficient % 1 == 0:
+                coefficient = int(coefficient)
+
+            output = ''
+
+            if coefficient < 0:
+                output += '-'
+            if coefficient > 0 and not is_initial_term:
+                output += '+'
+
+            if not is_initial_term:
+                output += ' '
+
+            if abs(coefficient) != 1:
+                output += '{}'.format(abs(coefficient))
+
+            return output
+
+        n = self.normal_vector
+
+        try:
+            initial_index = Plane.first_nonzero_index(n)
+            terms = [write_coefficient(n[i], is_initial_term=(i==initial_index)) + 'x_{}'.format(i+1)
+                     for i in range(self.dimension) if round(n[i], num_decimal_places) != 0]
+            output = ' '.join(terms)
+
+        except Exception as e:
+            if str(e) == self.NO_NONZERO_ELTS_FOUND_MSG:
+                output = '0'
+            else:
+                raise e
+
+        constant = round(self.constant_term, num_decimal_places)
+        if constant % 1 == 0:
+            constant = int(constant)
+        output += ' = {}'.format(constant)
+
+        return output
+
+
+    @staticmethod
+    def first_nonzero_index(iterable):
+        for k, item in enumerate(iterable):
+            if not MyDecimal(item).is_near_zero():
+                return k
+        raise Exception(Plane.NO_NONZERO_ELTS_FOUND_MSG)
 
     def parallel_to(self, l):
         return self.normal_vector.is_parallel(l.normal_vector)
@@ -73,6 +132,10 @@ class Plane:
                 raise e
 
 
+class MyDecimal(Decimal):
+    def is_near_zero(self, eps=1e-10):
+        return abs(self) < eps
+
 p1 = Plane(Vector([-0.412, 3.806, 0.728]), -3.46)
 p2 = Plane(Vector([1.03, -9.515, -1.82]), 8.65)
 equal = p1==p2
@@ -91,4 +154,5 @@ p2 = Plane(Vector([-2.642, 2.875, -2.404]), -2.443)
 equal = p1==p2
 parallel = p1.parallel_to(p2)
 print(f'#3 is EQUAL: {equal}, and PARALLEL: {parallel}')
+
 
